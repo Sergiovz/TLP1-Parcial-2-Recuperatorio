5208c7429914a96ee1cd4576c8a0074017fe0573
sergio valdez <sergiovaldez599@gmail.com>
2023-07-05 16:52:49 -0300

    modelado de datos

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,66 @@
 // TODO: Crear modelo de datos de Reserva
+const { DataTypes, sequelize } = require('../database');
+
+const Reserva = sequelize.define('Reserva', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    codigo: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    fecha_entrada: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    fecha_salida: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    costo_pasaje: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dni: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reservasVuelos'
+});
+
+
+//crea una tabla nueva 
+Reserva.sync({ force: false }).then(() => {
+    console.log('Creación de tablas de Reservas');
+});
+
+module.exports = Reserva;
--- routes/reserva.routes.js
+++ routes/reserva.routes.js
@@ -1,33 +1,37 @@
 // TODO: Importar el modelo y controladores de reservas, luego vincular rutas con controladores
-
-const router = require('express').Router();
-
+const { Router } = require('express');
+const { obtenerReservas, crearReserva, actualizarReserva, eliminarReserva, renderListaReservas, renderFormNuevaReserva, renderFormEditarReserva, obtenerReserva } = require('../controllers/reserva.controllers');
+const router = Router();
 
 // ==========================================
 //         Rutas para renderizar vistas
 // ==========================================
 
 // Obtener todas las reservas
-
+router.get('/', renderListaReservas);
 // Formulario para crear una reserva
-
+router.get('/crear-reserva', renderFormNuevaReserva);
 // Formulario para actualizar una reserva
+router.get('/actualizar-reserva/:id', renderFormEditarReserva); // para capturar :id -> req.params.id
+
 
 // ==========================================
 //         Rutas para CRUD de reservas
 // ==========================================
 
 // Obtener todas las reservas
-router.get('/api/',);
- 
+router.get('/api', obtenerReservas);
+
+// Obtener un id
+router.get('/api/:id', obtenerReserva);
+
 // Crear una reserva
-router.post('/api/',);
+router.post('/api', crearReserva);
  
 // Actualizar una reserva
-router.put('/api/:id',);
+router.put('/api/:id', actualizarReserva);
  
 // Eliminar una reserva de forma lógica
-router.delete('/api/:id',);
-
+router.delete('/api/:id', eliminarReserva);
  
  module.exports = router;
