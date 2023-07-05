6dfa922acb2549fc6a0d794daab0257387add4b3
sergio valdez <sergiovaldez599@gmail.com>
2023-07-05 16:37:43 -0300

    condiguracion del servidor

---
+++ README.md
@@ -0,0 +1,16 @@
+#EXAMEN DE TPL1-Parcial-2-Recuperatorio
+
+Instalación:
+npm install o npm i
+
+Configuración de Variables de Entorno:
+PORT=
+DB_NAME=
+DB_USER=
+DB_PASSWORD=
+DB_HOST=
+DB_PORT=
+DB_DIALECT=
+
+Ejecución:
+npm run dev
--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
+require('ejs');
 
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(express.json());
+app.use(helmet);
+app.use(cors);
+app.use(morgan('dev'));
+
+app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Servidor conectado en http:localhost: ${port}`));
